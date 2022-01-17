415247abfe8e4a27508894f7183e9546115d58b2
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2022-01-17 14:28:56 -0600

    Update iphone.js

--- iphone.js
+++ iphone.js
@@ -1,6 +1,32 @@
-setInterval(function(){
+
+styl();// for all
+
+setInterval(function(){styl();},200);
+
+function styl(){
+
+    if(!document.getElementById('sty')){
+var sty=document.createElement('style');
+sty.id='sty';
+sty.innerHTML='';
+
+sty.innerHTML=sty.innerHTML+' [id*="google_ads"]{display:none;visibility:hidden;}';
+sty.innerHTML=sty.innerHTML+' [class*="sponsored"]{display:none;visibility:hidden;}';
+
+sty.innerHTML=sty.innerHTML+' .premium-banner-outer{display:none;visibility:hidden;}';
+sty.innerHTML=sty.innerHTML+' .justify-content-center{display:none;visibility:hidden;}';
+sty.innerHTML=sty.innerHTML+' #divcookie{display:none;visibility:hidden;}';
+sty.innerHTML=sty.innerHTML+' div[id*="adBreak"]{display:none;visibility:hidden;}';
+sty.innerHTML=sty.innerHTML+' div[id*="primis"]{display:none;visibility:hidden;}';
+sty.innerHTML=sty.innerHTML+' div[id*="primis_player"]{display:none;visibility:hidden;}';
+sty.innerHTML=sty.innerHTML+' div[id*="primis_container_div"]{display:none;visibility:hidden;}';
+        sty.innerHTML=sty.innerHTML+' #ctl00_cite{display:none;visibility:hidden;}';
+                sty.innerHTML=sty.innerHTML+' #frb-main{display:none;visibility:hidden;}';
+          sty.innerHTML=sty.innerHTML+' #frb-inline{display:none;visibility:hidden;max-height:0px;max-witdh:0px;}';
+         sty.innerHTML=sty.innerHTML+' #frb-nag{display:none;visibility:hidden;max-height:0px;max-witdh:0px;}';
 
 
-document.body.style.filter="sepia(100%)";
 
-},1000);
+document.body.appendChild(sty);
+    }
+}
