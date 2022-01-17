13c778ea1d4960212a15e33c402c1f3e7a5fd95c
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2022-01-17 14:57:57 -0600

    Update iphone.js

--- iphone.js
+++ iphone.js
@@ -30,3 +30,59 @@ sty.innerHTML=sty.innerHTML+' div[id*="primis_container_div"]{display:none;visib
 document.body.appendChild(sty);
     }
 }
+
+
+
+
+if(document.domain.indexOf('reddit')>-1){// reddit
+
+var sty=document.createElement('style');
+
+
+sty.innerHTML='div.ad-banner{display:none;visibility:hidden;}';
+
+document.body.appendChild(sty);
+
+hidePromoted();
+
+setInterval(function(){hidePromoted();},2000);
+
+function hidePromoted(){
+
+    var spans=document.getElementsByTagName('span');
+
+    for(var i=0;i<spans.length;i++){
+
+        if(spans[i].innerHTML=='promoted'){
+    spans[i].parentElement.parentElement.parentElement.parentElement.parentElement.style.display='none';
+    }
+
+    }
+
+        var h3=document.getElementsByTagName('h3');
+
+    for(var j=0;j<h3.length;j++){
+
+        if(h3[j].innerHTML=='Top livestream'){
+    h3[j].parentElement.parentElement.parentElement.parentElement.parentElement.style.display='none';
+    }
+
+    }
+
+            var di=document.getElementsByTagName('div');
+
+    for(var k=0;k<di.length;k++){
+
+        if(di[k].innerHTML=='Reddit Premium'){
+    di[k].parentElement.parentElement.parentElement.parentElement.style.display='none';
+    }
+
+    }
+
+
+
+}
+
+
+
+}
