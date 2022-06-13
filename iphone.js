eb237978b5dbeeabaaa74314c6c69452914a18c0
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2022-06-13 11:38:01 -0500

    Update iphone.js

--- iphone.js
+++ iphone.js
@@ -44,7 +44,7 @@ for(var i=0;i<stuff.length;i++){
 var compStyles = window.getComputedStyle(stuff[i]);
 
     if(compStyles.getPropertyValue('background').indexOf('rgb(255, 255, 255)'>=1)) {stuff[i].style.backgroundColor='#1A1A1B';stuff[i].style.color='white';}
-
+ if(stuff[i].textContent.length>0) {stuff[i].style.color='white';}
 }
 }
 
