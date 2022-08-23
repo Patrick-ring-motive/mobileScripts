8a8a19f41b94af520385b707fedbf4a8b6c19d3b
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2022-08-23 14:51:28 -0500

    Update serviceWorkerProxy.js

--- serviceWorker/serviceWorkerProxy.js
+++ serviceWorker/serviceWorkerProxy.js
@@ -52,16 +52,23 @@ self.addEventListener('fetch', event => {
   // Skip cross-origin requests, like those for Google Analytics.
   if (event.request.url.startsWith(self.location.origin)) {
     event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+        if (cachedResponse) {
+         console.log(cachedResponse.headers);
+          return cachedResponse;
+        }
 
-           fetch(event.request).then(response => {//2
+        return caches.open(RUNTIME).then(cache => {
+          return fetch(event.request).then(response => {
             // Put a copy of the response in the runtime cache.
            var responseClone = response.clone();
            console.log(responseClone);
-            return cache.put(event.request, responseClone ).then(() => {//1
+            return cache.put(event.request, responseClone ).then(() => {
               return responseClone;
-            });//1
-          });//2
-
+            });
+          });
+        });
+      })
     );
   }
 });
