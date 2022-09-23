7433866926109e3bd692f82cb801ff3ca716db66
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2022-09-22 21:57:44 -0500

    Update redditAdblock.js

--- userScripts/iphone/reddit.com/redditAdblock.js
+++ userScripts/iphone/reddit.com/redditAdblock.js
@@ -1,26 +1,4 @@
-redditDark();
-setInterval(async function(){redditDark();},500);
 
-async function redditDark(){
-//reddit darkmode
-if((document.domain.indexOf('reddit')>-1)){
-	
-	var stuff = document.getElementsByTagName('*');
-
-for(var i=0;i<stuff.length;i++){
-var compStyles = window.getComputedStyle(stuff[i]);
-if(stuff[i].tagName=='A'){stuff[i].style.color='white';continue;}
-
-    if(compStyles.getPropertyValue('background').indexOf('rgb(255, 255, 255)'>=1)) {
-		
-		stuff[i].style.backgroundColor='#1A1A1B';stuff[i].style.color='white';
-		}
- if(stuff[i].textContent.length>0) {stuff[i].style.color='white';}
-}
-}
-
-
-}
 
 if((document.domain.indexOf('reddit')>-1)&&(window.location.href.indexOf('comments')==-1)){// reddit
 
