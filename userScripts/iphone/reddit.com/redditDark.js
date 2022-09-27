42d1740510b394b8d31bee7fe02a2bbc8deda516
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2022-09-27 08:46:09 -0500

    Update redditDark.js

--- userScripts/iphone/reddit.com/redditDark.js
+++ userScripts/iphone/reddit.com/redditDark.js
@@ -1,1 +1,13 @@
 persistScript('https://mobile-scripts.vercel.app/userScripts/iphone/reddit.com/redditDark.css','link');
+
+doInterval(async function(){
+  
+  
+  if(document.querySelector('span[style*="filter: none;"]')){
+  
+    document.querySelector('span[style*="filter: none;"]').style.removeProperty('filter');
+    
+  }
+                           
+                           
+},501);
