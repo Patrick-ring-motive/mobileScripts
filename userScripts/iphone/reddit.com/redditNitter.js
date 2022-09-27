858649fd13a5d4a6908f16900f3b8956e9ee022e
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2022-09-27 09:48:58 -0500

    Update redditNitter.js

--- userScripts/iphone/reddit.com/redditNitter.js
+++ userScripts/iphone/reddit.com/redditNitter.js
@@ -9,6 +9,7 @@ let lnks=document.querySelectorAll('a[href*="twitter.com"]');
   for(let i=0;i<lnks_length;i++){
   
   lnks[i].href=lnks[i].href.replaceAll('twitter.com','nitter.net');
+    lnks[i].target="_blank";
   
   }
 
