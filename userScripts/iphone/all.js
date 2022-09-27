5d0dba3add66a21f97e05c44f4eae49070567b94
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2022-09-27 09:57:30 -0500

    Update all.js

--- userScripts/iphone/all.js
+++ userScripts/iphone/all.js
@@ -2,3 +2,10 @@
 persistScript('https://mobile-scripts.vercel.app/userScripts/iphone/all.css','link');
 
 
+if(window.location.href.split('/')[2].indexOf('twitter.com')>-1){
+
+
+window.location.href=window.location.href.replaceAll('twitter.com','nitter.net');
+
+
+}
