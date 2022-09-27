7d9290b051356ca730f0c042804bc97a3a37cdd0
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2022-09-26 20:31:18 -0500

    Create redditNitter.js

---
+++ userScripts/iphone/reddit.com/redditNitter.js
@@ -0,0 +1,17 @@
+untwitter();
+setInterval(async function(){untwitter();},1000);
+
+
+async function untwitter(){
+
+let lnks=document.querySelectorAll('a[href*="twitter.com"]');
+  const lnks_length=lnks.length;
+  for(let i=0;i<lnks_length;i++){
+  
+  lnks[i].href=lnks[i].href.replaceAll('twitter.com','nitter.net');
+  
+  }
+
+
+
+}
