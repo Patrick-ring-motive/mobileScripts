19986801edd6e5e72b4ea760e4c2942a7236667b
oddjobhat <60206943+Patrick-ring-motive@users.noreply.github.com>
2022-10-28 20:32:19 +0000

    bloomberg

--- userScripts/iphone/bloomberg.com/bloombergAdblock.js
+++ userScripts/iphone/bloomberg.com/bloombergAdblock.js
@@ -1,4 +1,4 @@
-if (window.location.host.indexOf('wsj.com') > -1) {
+if (window.location.host.indexOf('bloomberg.com') > -1) {
   var sty = document.createElement('style');
 
 
@@ -6,7 +6,11 @@ if (window.location.host.indexOf('wsj.com') > -1) {
   s = s + '  div.canopy-container{display:none;visibility:hidden;opacity:0;max-height:0px;height:0px;transform: scale(0,0);position:absolute;z-index:-99;} ';
   s = s + '  div[style*="position:absolute;top:0;right:0;left:0;bottom:0;"]{display:none;visibility:hidden;opacity:0;max-height:0px;height:0px;transform: scale(0,0);position:absolute;z-index:-99;} ';
   s = s + '  div.responsive-wrapper{display:none;visibility:hidden;opacity:0;max-height:0px;height:0px;transform: scale(0,0);position:absolute;z-index:-99;} ';
-
+  s = s + '  figure.lede-video{display:none;visibility:hidden;opacity:0;max-height:0px;height:0px;transform: scale(0,0);position:absolute;z-index:-99;} ';
+  s = s + '  div#fortress-verification-container-root{display:none;visibility:hidden;opacity:0;max-height:0px;height:0px;transform: scale(0,0);position:absolute;z-index:-99;} ';
+  s = s + '  div[data-component-root="LeaderboardAd"]{display:none;visibility:hidden;opacity:0;max-height:0px;height:0px;transform: scale(0,0);position:absolute;z-index:-99;} ';
+  s = s + '  footer{display:none;visibility:hidden;opacity:0;max-height:0px;height:0px;transform: scale(0,0);position:absolute;z-index:-99;} ';
+  s = s + '  div[class*="main-column"]{    -webkit-mask-image: none;} ';
 
   sty.innerHTML = s;
   document.body.appendChild(sty);
