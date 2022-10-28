68c2475ab2b53bb3103aec552a464ebbeda9c793
oddjobhat <60206943+Patrick-ring-motive@users.noreply.github.com>
2022-10-28 20:08:26 +0000

    wsj

--- userScripts/iphone/wsj.com/wsjAdblock.js
+++ userScripts/iphone/wsj.com/wsjAdblock.js
@@ -2,12 +2,17 @@ if (window.location.host.indexOf('wsj.com') > -1) {
   var sty = document.createElement('style');
 
 
-var s='';
-  s=s+ '  div#wrapper-AD_MOBILE2{display:none;visibility:hidden;opacity:0;max-height:0px;height:0px;transform: scale(0,0);position:absolute;z-index:-99;} ';
-  s=s+ '  div#cx-snippet-overlay{display:none;visibility:hidden;opacity:0;max-height:0px;height:0px;transform: scale(0,0);position:absolute;z-index:-99;} ';
-  s=s+ '  div.css-1vlbiim-Container{display:none;visibility:hidden;opacity:0;max-height:0px;height:0px;transform: scale(0,0);position:absolute;z-index:-99;} ';
-
-  s=s+ '  section:after{max-height:0px;} ';
+  var s = '';
+  s = s + '  div#wrapper-AD_MOBILE2{display:none;visibility:hidden;opacity:0;max-height:0px;height:0px;transform: scale(0,0);position:absolute;z-index:-99;} ';
+  s = s + '  div#cx-snippet-overlay{display:none;visibility:hidden;opacity:0;max-height:0px;height:0px;transform: scale(0,0);position:absolute;z-index:-99;} ';
+  s = s + '  div.css-1vlbiim-Container{display:none;visibility:hidden;opacity:0;max-height:0px;height:0px;transform: scale(0,0);position:absolute;z-index:-99;} ';
+  s = s + '  div[aria-label="What to Read Next"]{display:none;visibility:hidden;opacity:0;max-height:0px;height:0px;transform: scale(0,0);position:absolute;z-index:-99;} ';
+  s = s + '  div[aria-label="Sponsored Offers"]{display:none;visibility:hidden;opacity:0;max-height:0px;height:0px;transform: scale(0,0);position:absolute;z-index:-99;} ';
+  s = s + '  div[data-block="doNotPrint"]{display:none;visibility:hidden;opacity:0;max-height:0px;height:0px;transform: scale(0,0);position:absolute;z-index:-99;} ';
+  s = s + '  div.adWrapper{display:none;visibility:hidden;opacity:0;max-height:0px;height:0px;transform: scale(0,0);position:absolute;z-index:-99;} ';
+  s = s + '  footer{display:none;visibility:hidden;opacity:0;max-height:0px;height:0px;transform: scale(0,0);position:absolute;z-index:-99;} ';
+  s = s + '    div[class*="LoginButtonsWrapper"]{display:none;visibility:hidden;opacity:0;max-height:0px;height:0px;transform: scale(0,0);position:absolute;z-index:-99;} ';
+  s = s + '  section:after{max-height:0px;} ';
   sty.innerHTML = s;
   document.body.appendChild(sty);
 }
