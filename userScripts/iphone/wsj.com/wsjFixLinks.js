1168bedd380cad002fe6870492ccce2983f602f0
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2022-11-11 15:19:01 -0600

    Update wsjFixLinks.js

--- userScripts/iphone/wsj.com/wsjFixLinks.js
+++ userScripts/iphone/wsj.com/wsjFixLinks.js
@@ -1,1 +1,12 @@
 
+setInterval(async function(){
+
+let lnks=document.querySelectorAll('a[href*="wsj.com"]');
+for(let i=0;i<lnks.length;i++){
+
+lnks[i].href=lnks[i].href.replace('www.wsj.com','wsj.webserve.workers.dev');
+
+}
+
+
+},1000);
