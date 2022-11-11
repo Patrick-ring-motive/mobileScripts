998133bbae380f7b1539e540c658b1a59bb4a64a
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2022-11-10 18:26:20 -0600

    Update wsjAdblock.js

--- userScripts/iphone/wsj.com/wsjAdblock.js
+++ userScripts/iphone/wsj.com/wsjAdblock.js
@@ -10,7 +10,7 @@ if (window.location.host.indexOf('wsj.com') > -1) {
   s = s + '  div[aria-label="Sponsored Offers"]{display:none;visibility:hidden;opacity:0;max-height:0px;height:0px;transform: scale(0,0);position:absolute;z-index:-99;} ';
   s = s + '  div[data-block="doNotPrint"]{display:none;visibility:hidden;opacity:0;max-height:0px;height:0px;transform: scale(0,0);position:absolute;z-index:-99;} ';
   s = s + '  div.adWrapper{display:none;visibility:hidden;opacity:0;max-height:0px;height:0px;transform: scale(0,0);position:absolute;z-index:-99;} ';
- // s = s + '  footer{display:none;visibility:hidden;opacity:0;max-height:0px;height:0px;transform: scale(0,0);position:absolute;z-index:-99;} ';
+  s = s + '  footer{display:none;visibility:hidden;opacity:0;max-height:0px;height:0px;transform: scale(0,0);position:absolute;z-index:-99;} ';
   s = s + '    div[class*="LoginButtonsWrapper"]{display:none;visibility:hidden;opacity:0;max-height:0px;height:0px;transform: scale(0,0);position:absolute;z-index:-99;} ';
  // s = s + '  section:after{max-height:0px;} ';
   sty.innerHTML = s;
