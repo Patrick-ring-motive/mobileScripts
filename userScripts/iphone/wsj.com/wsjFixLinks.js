8e85ab7924efaee1c7b1b93e507b8dbffcb2113b
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2022-11-11 22:19:06 -0600

    Update wsjFixLinks.js

--- userScripts/iphone/wsj.com/wsjFixLinks.js
+++ userScripts/iphone/wsj.com/wsjFixLinks.js
@@ -1,6 +1,7 @@
 
 setInterval(async function(){
   
+  
 let lnks=document.querySelectorAll('a[href*="deloitte.wsj.com"]');
 for(let i=0;i<lnks.length;i++){
 
@@ -14,7 +15,11 @@ for(let i=0;i<lnks.length;i++){
 lnks[i].href=lnks[i].href.replace('www.wsj.com','wsj.webserve.workers.dev');
 
 }
+if(document.querySelector('a[href*="wsjshop.com/"]')){
 
-
+   let dswj =document.querySelector('a[href*="wsjshop.com/"]');
+   dswj.href="https://dwsj.webserve.workers.dev/";
+   dswj.innerText='Insights';
+}
 
 },1000);
