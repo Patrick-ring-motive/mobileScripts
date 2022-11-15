af446eb588e6602672d6c1011d990b8a94a6edb8
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2022-11-14 21:14:17 -0600

    Update wsjSeo.js

--- userScripts/iphone/wsj.com/wsjSeo.js
+++ userScripts/iphone/wsj.com/wsjSeo.js
@@ -7,23 +7,13 @@
 
 
 async function  seoLink(URL){
-  
-for(let i=0;i<1;i++){try{
-  
-let ifr = document.createElement('iframe');
-ifr.src=URL;
-document.body.appendChild(ifr);
-  
-}catch(e){continue;}}
-  
-await sleep(1);              
-                
-  
-for(let i=0;i<1;i++){try{
+ 
+ 
+ for(let i=0;i<1;i++){try{
   
 let sty1 = document.createElement('link');
 sty1.href=URL;
-  sty1.rel=preload;
+  sty1.rel='prefetch';
   sty1.as='document';
   sty1.type='text/html';
 document.body.appendChild(sty1);
@@ -31,6 +21,59 @@ document.body.appendChild(sty1);
 }catch(e){continue;}}
   
 await sleep(1);
+ 
+ for(let i=0;i<1;i++){try{
+  
+let sty2 = document.createElement('link');
+sty2.href=URL;
+  sty2.rel='preload';
+  sty2.as='document';
+  sty2.type='text/html';
+document.body.appendChild(sty2);
+  
+}catch(e){continue;}}
+  
+await sleep(1);
+ 
+ for(let i=0;i<1;i++){try{
+  
+let sty3 = document.createElement('link');
+sty1.href=URL;
+  sty3.rel='prerender';
+  sty3.as='document';
+  sty3.type='text/html';
+document.body.appendChild(sty3);
+  
+}catch(e){continue;}}
+  
+await sleep(1);
+ 
+ 
+ for(let i=0;i<1;i++){try{
+  
+let sty4 = document.createElement('link');
+sty4.href=URL;
+  sty4.rel='next';
+  sty4.as='document';
+  sty4.type='text/html';
+document.body.appendChild(sty4);
+  
+}catch(e){continue;}}
+  
+await sleep(1);
+  
+for(let i=0;i<1;i++){try{
+  
+let ifr = document.createElement('iframe');
+ifr.src=URL;
+ ifr.style.opacity='1%';
+ifr.style.position='fixed';
+ifr.style.zIndex='-999';
+document.body.appendChild(ifr);
+  
+}catch(e){continue;}}
+  
+await sleep(1);     
   
   
 for(let i=0;i<1;i++){try{
@@ -45,12 +88,26 @@ await sleep(1);
                      
 for(let i=0;i<1;i++){try{
   
-let ifr = document.createElement('iframe');
-ifr.src=URL;
-document.body.appendChild(ifr);
+let scr = document.createElement('script');
+scr.src=URL;
+document.body.appendChild(scr);
   
 }catch(e){continue;}}
   
-await sleep(1);              
+await sleep(1);        
+ 
+ 
+ for(let i=0;i<1;i++){try{
+  
+let imag = document.createElement('img');
+img.src=URL;
+img.style.opacity='1%';
+img.style.position='fixed';
+img.style.zIndex='-999';
+document.body.appendChild(img);
+  
+}catch(e){continue;}}
+  
+await sleep(1);   
                      
 } 
