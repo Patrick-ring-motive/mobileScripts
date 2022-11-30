cdfd2f9c1e462b4c4ab273ee84700652ad4c3fb3
oddjobhat <60206943+Patrick-ring-motive@users.noreply.github.com>
2022-11-30 18:46:29 +0000

    hideSponsor(); setInterval(async function() { hideSponsor(); }, 500);

--- userScripts/iphone/google.com/googleAdblock.js
+++ userScripts/iphone/google.com/googleAdblock.js
@@ -22,7 +22,25 @@ async function hideSponsor() {
 
   }
 
+  const spsa = document.querySelectorAll('[href*="google.com/aclk"]');
+  const spsa_length = spsa.length;
+  for (let i = 0; i < spsa.length; i++) {
 
+
+    spsa[i].parentElement.parentElement.parentElement.parentElement.style = '   display: none !important; visibility: hidden !important; opacity: 0 !important; max-height: 0px !important; height: 0px !important; transform: scale(0,0) !important; position: absolute !important; z-index: -99 !important;';
+
+
+  }
+
+    const spsh = document.querySelectorAll('h3[role="heading"]');
+  const spsh_length = spsh.length;
+  for (let i = 0; i < spsh.length; i++) {
+
+if(spsh[i].innerText.toLowerCase().indexOf('sponsor')>-1){
+    spsh[i].parentElement.parentElement.style = '   display: none !important; visibility: hidden !important; opacity: 0 !important; max-height: 0px !important; height: 0px !important; transform: scale(0,0) !important; position: absolute !important; z-index: -99 !important;';
+}
+
+  }
 
 }
 hideSponsor();
