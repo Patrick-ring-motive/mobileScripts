a16d49608702c2f1bfc3cc5c2a61759b1b0516b5
oddjobhat <60206943+Patrick-ring-motive@users.noreply.github.com>
2022-11-30 18:51:47 +0000

    _

--- userScripts/iphone/google.com/googleAdblock.js
+++ userScripts/iphone/google.com/googleAdblock.js
@@ -3,7 +3,7 @@ async function hideSponsor() {
 
   const sps = document.querySelectorAll('[id*="gws-ads"]');
   const sps_length = sps.length;
-  for (let i = 0; i < sps.length; i++) {
+  for (let i = 0; i < sps_length; i++) {
 
 
     sps[i].parentElement.parentElement.parentElement.parentElement.style = '   display: none !important; visibility: hidden !important; opacity: 0 !important; max-height: 0px !important; height: 0px !important; transform: scale(0,0) !important; position: absolute !important; z-index: -99 !important;';
@@ -14,7 +14,7 @@ async function hideSponsor() {
 
   const spsw = document.querySelectorAll('w-ad-creative-spacer');
   const spsw_length = spsw.length;
-  for (let i = 0; i < spsw.length; i++) {
+  for (let i = 0; i < spsw_length; i++) {
 
 
     spsw[i].parentElement.style = '   display: none !important; visibility: hidden !important; opacity: 0 !important; max-height: 0px !important; height: 0px !important; transform: scale(0,0) !important; position: absolute !important; z-index: -99 !important;';
@@ -24,7 +24,7 @@ async function hideSponsor() {
 
   const spsa = document.querySelectorAll('[href*="google.com/aclk"]');
   const spsa_length = spsa.length;
-  for (let i = 0; i < spsa.length; i++) {
+  for (let i = 0; i < spsa_length; i++) {
 
 
     spsa[i].parentElement.parentElement.parentElement.parentElement.style = '   display: none !important; visibility: hidden !important; opacity: 0 !important; max-height: 0px !important; height: 0px !important; transform: scale(0,0) !important; position: absolute !important; z-index: -99 !important;';
@@ -32,13 +32,24 @@ async function hideSponsor() {
 
   }
 
-    const spsh = document.querySelectorAll('h3[role="heading"]');
+  const spsh = document.querySelectorAll('h3[role="heading"]');
   const spsh_length = spsh.length;
-  for (let i = 0; i < spsh.length; i++) {
+  for (let i = 0; i < spsh_length; i++) {
 
-if(spsh[i].innerText.toLowerCase().indexOf('sponsor')>-1){
-    spsh[i].parentElement.parentElement.style = '   display: none !important; visibility: hidden !important; opacity: 0 !important; max-height: 0px !important; height: 0px !important; transform: scale(0,0) !important; position: absolute !important; z-index: -99 !important;';
-}
+    if (spsh[i].innerText.toLowerCase().indexOf('sponsor') > -1) {
+      spsh[i].parentElement.parentElement.style = '   display: none !important; visibility: hidden !important; opacity: 0 !important; max-height: 0px !important; height: 0px !important; transform: scale(0,0) !important; position: absolute !important; z-index: -99 !important;';
+    }
+
+  }
+
+
+  const spsg = document.querySelectorAll('[aria-label="Why these sponsored results?"]');
+  const spsg_length = spsg.length;
+  for (let i = 0; i < spsg_length; i++) {
+
+
+    spsg[i].parentElement.parentElement.style = '   display: none !important; visibility: hidden !important; opacity: 0 !important; max-height: 0px !important; height: 0px !important; transform: scale(0,0) !important; position: absolute !important; z-index: -99 !important;';
+
 
   }
 
