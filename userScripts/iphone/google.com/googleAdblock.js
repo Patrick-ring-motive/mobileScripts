bfa3d7589d5368fa6fc90efdf9da37ed95a63aef
oddjobhat <60206943+Patrick-ring-motive@users.noreply.github.com>
2022-11-30 18:30:09 +0000

    async function hideSponsor() {
      const sps = document.querySelectorAll('[id*="gws-ads"]');
      const sps_length = sps.length;
      for (let i = 0; i < sps.length; i++) {
    
        sps[i].parentElement.parentElement.parentElement.parentElement.style = '   display: none !important; visibility: hidden !important; opacity: 0 !important; max-height: 0px !important; height: 0px !important; transform: scale(0,0) !important; position: absolute !important; z-index: -99 !important;';
    
      }
    
      const spsw = document.querySelectorAll('w-ad-creative-spacer');
      const spsw_length = spsw.length;
      for (let i = 0; i < spsw.length; i++) {
    
        spsw[i].parentElement.style = '   display: none !important; visibility: hidden !important; opacity: 0 !important; max-height: 0px !important; height: 0px !important; transform: scale(0,0) !important; position: absolute !important; z-index: -99 !important;';
    
      }
    
    }
    hideSponsor();
    setInterval(async function() { hideSponsor(); }, 500);

--- userScripts/iphone/google.com/googleAdblock.js
+++ userScripts/iphone/google.com/googleAdblock.js
@@ -2,7 +2,7 @@
 async function hideSponsor() {
 
   const sps = document.querySelectorAll('[id*="gws-ads"]');
-  const sps_length = sps.lengthk
+  const sps_length = sps.length;
   for (let i = 0; i < sps.length; i++) {
 
 
@@ -12,6 +12,15 @@ async function hideSponsor() {
   }
 
 
+  const spsw = document.querySelectorAll('w-ad-creative-spacer');
+  const spsw_length = spsw.length;
+  for (let i = 0; i < spsw.length; i++) {
+
+
+    spsw[i].parentElement.style = '   display: none !important; visibility: hidden !important; opacity: 0 !important; max-height: 0px !important; height: 0px !important; transform: scale(0,0) !important; position: absolute !important; z-index: -99 !important;';
+
+
+  }
 
 
 
